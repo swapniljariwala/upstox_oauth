d2183438db5c2599ffaf5c9c956bdfc944b393c3
Swapnil <sjerry4u@gmail.com>
2018-09-25 23:19:16 +0530

    edited

--- upstox_auth/urls.py
+++ upstox_auth/urls.py
@@ -3,7 +3,11 @@ from django.urls import path, include
 from upstox_auth import views
 urlpatterns = [
     path('', views.home, name='home'),
-    path('login/', views.login, name='login'),
+    path('login/', views.login_upstox, name='login'),
     path('redirect/', views.redirect, name='redirect'),
-    path('readaccesstoken', views.readaccesstoken, name='readaccesstoken')
+    path('readaccesstoken/', views.readaccesstoken, name='readaccesstoken'),
+    path('djangologin/', views.djangologin, name='djangologin'),
+    path('djangologinsubmit/', views.djangologinsubmit, name='djangologinsubmit'),
+    path('djangologout/', views.djangologout, name='djangologout')
+    
 ]
--- upstox_auth/views.py
+++ upstox_auth/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from upstox_api import api
 from django.http import Http404
+from django.contrib.auth import authenticate, logout, login
 import dotenv
 # Create your views here.
 
@@ -17,12 +18,14 @@ def home(request):
     if not API_SECRET:
         alert_flag = True
         alert_text = alert_text + "Could not find API SECRET."
-    context = {"alert_flag": alert_flag, "alert_text": alert_text}
+    context = {"alert_flag": alert_flag, "alert_text": alert_text, "is_auth": request.user.is_authenticated}
 
     return render(request, 'upstox_auth/index.html', context=context)
 
 
-def login(request):
+def login_upstox(request):
+    if not request.user.is_authenticated:
+        HttpResponseRedirect("{}?alert=unauthorized".format(reverse('djangologin')))
     API_KEY = os.getenv('API_KEY')
     API_SECRET = os.getenv('API_SECRET')
     s = api.Session(API_KEY)
@@ -30,6 +33,32 @@ def login(request):
     url = request.build_absolute_uri(reverse('redirect'))
     s.set_redirect_uri(url)
     return HttpResponseRedirect(s.get_login_url())
+
+def djangologin(request):
+    alert = request.GET.get('alert', False)
+    alert_text =""
+    if alert == "wrongpassword":
+        alert_text = "Wrong username or password"
+    elif alert == "loggedout":
+        alert_text = "Logged out successfully"
+    elif alert == "unauthorized":
+        alert_text = "Please login to access this resource"
+    context = {"alert_flag": alert , "alert_text": alert_text}
+    return render(request, 'upstox_auth/djangologin.html', context=context)
+
+def djangologinsubmit(request):
+    userid = request.POST.get('userid')
+    password = request.POST.get('password')
+    user = authenticate(username=userid, password=password)
+    if user is not None:
+        login(request, user)
+        return HttpResponseRedirect(reverse("home"))
+    else:
+        return HttpResponseRedirect("{}?alert=wrongpassword".format(reverse('djangologin')))
+
+def djangologout(request):
+    logout(request)
+    return HttpResponseRedirect("{}?alert=loggedout".format(reverse('djangologin')))
 
 def redirect(request):
     code = request.GET.get('code')
@@ -48,6 +77,11 @@ def redirect(request):
     return HttpResponseRedirect(reverse('readaccesstoken'))
 
 def readaccesstoken(request):
+    if not request.user.is_authenticated:
+        print("Loggedin")
+        return HttpResponseRedirect("{}?alert=unauthorized".format(reverse('djangologin')))
+    else:
+        print("Not authenticated")
     code = dotenv.get_key(dotenv.find_dotenv(), 'ACCESS_TOKEN')
     context = {'accesstoken': code}
     return render(request, 'upstox_auth/tokendisplay.html', context=context)
